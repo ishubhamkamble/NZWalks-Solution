a3825c885e87840ddfc94ea5df8324f7b023e6d7
Shubham Kamble <shubhkam61@gmail.com>
2025-05-28 12:53:57 +0530

    InMemory Repository Implemented

--- NzWalksAPI/Program.cs
+++ NzWalksAPI/Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
-builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+//builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+builder.Services.AddScoped<IRegionRepository, InMemoryRegionRepository>();
 
 var app = builder.Build();
 
---
+++ NzWalksAPI/Repositories/InMemoryRegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using NzWalksAPI.Models.Domain;
+
+namespace NzWalksAPI.Repositories
+{
+    public class InMemoryRegionRepository : IRegionRepository
+    {
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return new List<Region>()
+            {
+                new Region()
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "NRTL",
+                    Name = "NorthLand Region"
+                }
+            };
+        }
+    }
+}
