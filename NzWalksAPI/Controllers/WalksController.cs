03e015b9d2e328400738579f38bfae04bb7d108e
Shubham Kamble <shubhkam61@gmail.com>
2025-07-14 12:37:47 +0530

    Authentication implemented and tested without JWT Toekn

--- NzWalksAPI/Controllers/WalksController.cs
+++ NzWalksAPI/Controllers/WalksController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -11,6 +12,7 @@ namespace NzWalksAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class WalksController : ControllerBase
     {
         private readonly IMapper mapper;
