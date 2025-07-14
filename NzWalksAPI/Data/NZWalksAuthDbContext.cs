b919893896247fe5ddb95609ecc5da25a1b7ac5e
Shubham Kamble <shubhkam61@gmail.com>
2025-07-14 13:15:37 +0530

    Seeding Roles data and Migration for Creating Auth Database

--- NzWalksAPI/Data/NZWalksAuthDbContext.cs
+++ NzWalksAPI/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NzWalksAPI.Data
@@ -8,6 +9,32 @@ namespace NzWalksAPI.Data
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
             //Contructor
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        { 
+            base.OnModelCreating(builder);
+            var ReaderRoleId = "05125329-1002-488b-9707-8c7d00885838";
+            var writerRoleId = "f089b4bc-910e-4ad9-918e-2196aec5b048";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = ReaderRoleId,
+                    ConcurrencyStamp = ReaderRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
