cafd6854fc530605cb7a297208ce3dfaa239a489
Shubham Kamble <shubhkam61@gmail.com>
2025-08-11 18:03:50 +0530

    Implemented ImageController and saved result to Image folder

---
+++ NzWalksAPI/Controllers/ImagesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NzWalksAPI.Models.Domain;
+using NzWalksAPI.Models.DTO;
+using NzWalksAPI.Repositories;
+
+namespace NzWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        public IImageRepository ImageRepository;
+        public ImagesController(IImageRepository imageRepository)
+        {
+            ImageRepository = imageRepository;
+        }
+
+
+        //POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileupload(request);
+
+            if (ModelState.IsValid)
+            {
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileName = request.File.FileName,
+                    FileSizeinBytes = request.File.Length,
+                    FileDescription = request.FileDescription
+                };
+
+                //User Repository to Upload Image
+                await ImageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileupload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "UnSupported file extension");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File Szie is more than 10MB, please upload a smaller size file.");
+            }
+        }
+    }
+}
