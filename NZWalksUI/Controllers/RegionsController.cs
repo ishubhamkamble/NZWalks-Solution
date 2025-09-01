9eed6ac444a009e29ca40b5e07f86f9627c4da08
Shubham Kamble <shubhkam61@gmail.com>
2025-09-01 16:38:42 +0530

    Consume PUT() and DELETE() in application using WebAPI

--- NZWalksUI/Controllers/RegionsController.cs
+++ NZWalksUI/Controllers/RegionsController.cs
@@ -49,6 +49,7 @@ namespace NZWalksUI.Controllers
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Add(AddRegionViewModel addRegionViewModel)
         {
 
@@ -70,7 +71,63 @@ namespace NZWalksUI.Controllers
 
             }
 
-            return View(response);
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionsDto>($"https://localhost:7005/API/regions/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RegionsDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7005/API/regions/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionsDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Regions"); 
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionsDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7005/API/regions/{request.Id}");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions"); 
+            }
+            catch (Exception ex)
+            {
+                //log the exception
+            }
+            return View("Edit");
         }
     }
 }
