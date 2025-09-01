3647b3ba7bb5ba68d37418dd762a2d92ba0f560e
Shubham Kamble <shubhkam61@gmail.com>
2025-09-01 14:32:25 +0530

    Consume API with GET() for Region

---
+++ NZWalksUI/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalksUI.Models.DTO;
+using System.Threading.Tasks;
+
+namespace NZWalksUI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionsDto> response = new List<RegionsDto>();
+            try
+            {
+                //Get All regions from WebAPI
+                var client = httpClientFactory.CreateClient();
+                var httpResponseMessage = await client.GetAsync("https://localhost:7005/API/regions");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                //var stringResponseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+                //ViewBag.Response = stringResponseBody;
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionsDto>>());
+
+
+            }
+            catch (Exception ex)
+            {
+                //log the exception
+            }
+
+            return View(response);
+        }
+    }
+}
---
+++ NZWalksUI/Models/DTO/RegionsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalksUI.Models.DTO
+{
+    public class RegionsDto
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageURL { get; set; }
+    }
+}
